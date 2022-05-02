f05142b36deeeaef77150c668e5486816fbbfa8e
nmolanog <nmolanog@gmail.com>
2022-05-02 16:05:20 -0500

    another commit in branch2

--- some_script.R
+++ some_script.R
@@ -3,10 +3,13 @@
 #######################
 rm(list=ls())
 
-z0<-rnorm(100)
+z0<-rnorm(1000)
 z1<-runif(1000)
 z2<-rbeta(1000,shape1=2,shape2=2)
 
 m0<-lm(z1~z2)
 
 plot(z1,z2)
+
+m1<-lm(z0~z1)
+plot(z0,z1)
